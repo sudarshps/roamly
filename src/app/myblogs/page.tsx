0bc1712c4e735172b93fa6c2097608ffd3682a90
sudarsh <sudarshwazza382@gmail.com>
2025-03-11 16:18:03 +0530

    fallback

--- src/app/myblogs/page.tsx
+++ src/app/myblogs/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../ui/navbar";
 import useAxiosWithAuth from "@/lib/useAxiosWithAuth";
-import { useRouter } from "next/navigation";
 import Swal from "sweetalert2";
 import Footer from "../ui/footer";
 import { SkeletonDemo } from "../ui/skeletonList";
@@ -19,15 +18,14 @@ interface PostListType {
 
 const Page = () => {
   const axiosInstance = useAxiosWithAuth();
-  const router = useRouter();
   const [blogs, setBlogs] = useState<PostListType[]>([]);
-  const [loading,setLoading] = useState<boolean>(true)
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
     const fetchBlogList = async () => {
       const list = await axiosInstance.get("/api/post");
       if (list.data) {
         setBlogs(list.data.postList);
-        setLoading(false)
+        setLoading(false);
       }
     };
     fetchBlogList();
@@ -105,112 +103,116 @@ const Page = () => {
                     </th>
                   </tr>
                 </thead>
-                {loading?(<div className="p-4"><SkeletonDemo/></div>):(<tbody className="bg-white divide-y divide-gray-200">
-                  {blogs.map((blog, i) => (
-                    <tr key={i} className="hover:bg-gray-50 transition-colors">
-                      <td className="px-6 py-4 max-w-[200px] truncate whitespace-nowrap overflow-hidden font-medium text-gray-900">
-                        {blog.title}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="flex space-x-4">
-                          <button
-                            className="text-blue-600 hover:text-blue-800 font-medium flex items-center"
-                            onClick={() => router.push(`/article/${blog.id}`)}
-                          >
-                            <svg
-                              className="w-4 h-4 mr-1"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
+                {loading ? (
+                  <div className="p-4">
+                    <SkeletonDemo />
+                  </div>
+                ) : (
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {blogs.map((blog, i) => (
+                      <tr
+                        key={i}
+                        className="hover:bg-gray-50 transition-colors"
+                      >
+                        <td className="px-6 py-4 max-w-[200px] truncate whitespace-nowrap overflow-hidden font-medium text-gray-900">
+                          {blog.title}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex space-x-4">
+                            <Link href={`/article/${blog.id}`}>
+                              <button className="text-blue-600 hover:text-blue-800 font-medium flex items-center">
+                                <svg
+                                  className="w-4 h-4 mr-1"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke="currentColor"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                  />
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                  />
+                                </svg>
+                                View
+                              </button>
+                            </Link>
+                            <Link href={`/updateblog/${blog.id}`}>
+                              <button className="text-indigo-600 hover:text-indigo-800 font-medium flex items-center">
+                                <svg
+                                  className="w-4 h-4 mr-1"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  stroke="currentColor"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                                  />
+                                </svg>
+                                Edit
+                              </button>
+                            </Link>
+
+                            <button
+                              className="text-red-600 hover:text-red-800 font-medium flex items-center"
+                              onClick={() => handleDelete(blog.id)}
                             >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                              />
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                              />
-                            </svg>
-                            View
-                          </button>
-                          <Link href={`/updateblog/${blog.id}`}>
-                          <button
-                            className="text-indigo-600 hover:text-indigo-800 font-medium flex items-center"
-    
-                          >
-                            <svg
-                              className="w-4 h-4 mr-1"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                              />
-                            </svg>
-                            Edit
-                          </button>
-                          </Link>
-                          
-                          <button
-                            className="text-red-600 hover:text-red-800 font-medium flex items-center"
-                            onClick={() => handleDelete(blog.id)}
-                          >
-                            <svg
-                              className="w-4 h-4 mr-1"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                              />
-                            </svg>
-                            Delete
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>)}
+                              <svg
+                                className="w-4 h-4 mr-1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth={2}
+                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                />
+                              </svg>
+                              Delete
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                )}
               </table>
             </div>
 
             <div className="border-t border-gray-200 px-6 py-4 bg-gray-50 flex justify-end">
-              <button
-                className="inline-flex items-center px-4 py-2 bg-gradient-to-r from-orange-400 to-red-500 text-white font-medium rounded-lg shadow hover:from-red-500 hover:to-orange-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-200"
-                onClick={() => router.push("/createblog")}
-              >
-                <svg
-                  className="w-4 h-4 mr-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 4v16m8-8H4"
-                  />
-                </svg>
-                New Blog Post
-              </button>
+              <Link href={`/createblog`}>
+                <button className="inline-flex items-center px-4 py-2 bg-gradient-to-r from-orange-400 to-red-500 text-white font-medium rounded-lg shadow hover:from-red-500 hover:to-orange-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-200">
+                  <svg
+                    className="w-4 h-4 mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 4v16m8-8H4"
+                    />
+                  </svg>
+                  New Blog Post
+                </button>
+              </Link>
             </div>
           </div>
         </div>
